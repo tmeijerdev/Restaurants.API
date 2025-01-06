5d2bd065f07a44237b59762383620ab02d864547
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-01-06 16:02:00 +0100

    Adding logging to file + move configuration into file

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -12,13 +12,20 @@ builder.Services.AddControllers();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog((context, configuration) => 
-{
-    configuration
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
-});
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration)
+);
+
+// builder.Host.UseSerilog((context, configuration) => 
+// {
+//     //Rolling interface says when to use a new file
+//     configuration
+//         .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+//         .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+//         .WriteTo.File("Logs/Restaurant-API-.log", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+//         .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
+// });
 
 var app = builder.Build();
 
