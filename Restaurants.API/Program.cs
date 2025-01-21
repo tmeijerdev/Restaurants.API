891a5945fab7fa384030f8997a6e3148283d3c64
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-01-21 16:36:49 +0100

    Swagger bearer added

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -4,14 +4,33 @@ using Restaurants.Application.Extensions;
 using Serilog;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            }, 
+            []
+        }
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
 
@@ -58,7 +77,10 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapIdentityApi<User>();
+// Add prefix api/identity 
+app.MapGroup("api/identity").MapIdentityApi<User>();
+
+// app.MapIdentityApi<User>();
 
 app.UseAuthorization();
 
