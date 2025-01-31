c3c72983b5078a00fab67bbb2f863f71221cb41e
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-01-31 15:37:02 +0100

    Extracting logic in builder extension class

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -4,43 +4,17 @@ using Restaurants.Application.Extensions;
 using Serilog;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c => 
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
-            }, 
-            []
-        }
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
+
 
 // builder.Host.UseSerilog((context, configuration) => 
 // {
