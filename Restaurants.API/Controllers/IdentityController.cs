692b512b66dcb295dacd4959532a4b8a6996f7c5
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-02-03 11:19:18 +0100

    Assign user roles

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -2,6 +2,8 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands;
+using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Domain.Constants;
 
 namespace Restaurants.API.Controllers;
 
@@ -12,6 +14,14 @@ public class IdentityController(IMediator mediator) : ControllerBase
     [HttpPatch("user")]
     [Authorize]
     public async Task<IActionResult> UpdateUserDetails(UpdateUserDetailsCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
+
+    [HttpPost("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
     {
         await mediator.Send(command);
         return NoContent();
