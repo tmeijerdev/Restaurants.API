ba132d587c20d06fc063526ce92aefa736243cd2
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-02-03 12:24:18 +0100

    Implement custom user claims

--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Authorization;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -23,6 +24,7 @@ public static class ServiceCollectionExtensions
         // AddRoles to enable custom claims
         services.AddIdentityApiEndpoints<User>()
             .AddRoles<IdentityRole>()
+            .AddClaimsPrincipalFactory<RestaurantsUserClaimsPrincipalFactory>()
             .AddEntityFrameworkStores<RestaurantsDbContext>();
 
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
