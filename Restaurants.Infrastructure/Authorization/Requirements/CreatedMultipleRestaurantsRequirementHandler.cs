8c7346e27eebe7d1938cbd52e4428b5c22c575f4
Tristan Meijer <78483207+Red-Hulk@users.noreply.github.com>
2025-02-19 14:31:54 +0100

    Authorization tests completed

--- Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
+++ Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
@@ -5,7 +5,7 @@ using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Infrastructure.Authorization.Requirements;
 
-public class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
+internal class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
     IUserContext userContext) : AuthorizationHandler<CreatedMultipleRestaurantsRequirement>
 {
     protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, 
